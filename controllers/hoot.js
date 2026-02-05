23582673dd370e9d9643d02400cfd934075caeb1
John F <anabasisinc@outlook.com>
2026-02-05 12:04:07 -0500

    added Hoot index route

--- controllers/hoot.js
+++ controllers/hoot.js
@@ -6,7 +6,7 @@ const router = express.Router();
 /* 
 HTTP Method	Controller	Response	URI	Use Case
 
-GET	index	200	/hoots	List hoots
+
 GET	show	200	/hoots/:hootId	Get a single hoot
 PUT	update	200	/hoots/:hootId	Update a hoot
 DELETE	deleteHoot	200	/hoots/:hootId	Delete a hoot
@@ -34,7 +34,25 @@ router.post("/", verifyToken, async (req, res) => {
 
         const hoot = await Hoot.create(req.body)
 
+        hoot._doc.author = req.user
+
         res.status(201).json({ hoot })
+    } catch (error) {
+        res.status(500).json({ err: error.message })
+    }
+});
+
+// GET	index	200	/hoots	List hoots
+
+router.get("/", verifyToken, async (req, res) => {
+    try {
+
+        const hoots = await Hoot.find()
+
+            .populate("author")
+            .sort({ createdAt: "desc" })
+
+        res.status(200).json(hoots)
     } catch (error) {
         res.status(500).json({ err: error.message })
     }
